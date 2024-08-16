92dfb24614314fdbd32659e9226ad29b7a03ee02
Aayan Asim <aayan.asim23@gmail.com>
2024-08-15 22:44:30 -0400

    Functional Website

---
+++ Aaron/script3.js
@@ -0,0 +1,12 @@
+function togglePlayPause() {
+    const audio = document.getElementById('myAudio');
+    const button = document.querySelector('.control-button');
+  
+    if (audio.paused) {
+        audio.play();
+        button.textContent = 'Pause Music'; 
+    } else {
+        audio.pause();
+        button.textContent = 'Play Music';
+    }
+  }
--- Main Page/script.js
+++ Main Page/script.js
@@ -1,7 +1,10 @@
+var button = document.getElementById("navigateButton");
+function goLink(){
+    window.location.href = "https://docs.google.com/presentation/d/1kghlaqtHvn7N_Mt9oSPfPA9EVnQBwwfBWmH6LglNYLM/copy";
+}
 function togglePlayPause() {
     const audio = document.getElementById('myAudio');
     const button = document.querySelector('.control-button');
-  
     if (audio.paused) {
         audio.play();
         button.textContent = 'Pause Music'; 
